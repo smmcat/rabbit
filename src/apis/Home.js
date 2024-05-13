9810997b394276b0c16e93808e4f71318ea351f8
smmcat <1365116009@qq.com>
2024-05-13 21:00:57 +0800

    完成 新鲜好物、人气推荐、懒加载业务

--- src/apis/Home.js
+++ src/apis/Home.js
@@ -1,7 +1,30 @@
 import httpInstance from "@/utils/http";
 
+// 获取 轮播图 数据
 export const getBannerAPI = () => {
     return httpInstance({
         url: 'home/banner'
+    })
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+/**
+* @description: 获取人气推荐
+* @param {*}
+* @return {*}
+*/
+export const getHotAPI = () => {
+    return httpInstance({
+        url: 'home/hot'
     })
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,17 +4,12 @@ import HomeCategory from './components/HomeCategory.vue';
 import HomeProduct from './components/HomeProduct.vue';
 import HomeNew from './components/HomeNew.vue';
 import HomeHot from './components/HomeHot.vue';
-import HomePanel from './components/HomePanel.vue';
 </script>
 <template>
     <div class="container">
         <HomeCategory />
         <HomeBanner />
     </div>
-    <HomePanel title="新品好物" sub-title="新鲜好物 好多商品">
-        <div>我</div>
-    </HomePanel>
-    <HomePanel title="人气推荐" sub-title="人气推荐 好多商品"></HomePanel>
     <HomeNew />
     <HomeHot />
     <HomeProduct />
