051c8f0726d25c68ee6ca1f4a14f4e5e398996dc
smmcat <1365116009@qq.com>
2024-05-20 09:04:11 +0800

    home-goodsItem 组件封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -2,23 +2,25 @@
 import { useIntersectionObserver } from '@vueuse/core'
 
 export const lazyPlugin = {
-  install (app) {
+  install(app) {
     // 懒加载指令逻辑
     app.directive('img-lazy', {
-      mounted (el, binding) {
+      mounted(el, binding) {
         // el: 指令绑定的那个元素 img
         // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
         console.log(el, binding.value)
         const { stop } = useIntersectionObserver(
           el,
-          ([{ isIntersecting }]) => {
-            console.log(isIntersecting)
-            if (isIntersecting) {
-              // 进入视口区域
+          (entries) => {
+            // 如果进度到视野区域 回调函数的下方参数返回 true
+            if (entries[0].isIntersecting) {
+              console.log(entries[0].isIntersecting);
+              // 修改图片的 src 的值 实现延迟加载图片
               el.src = binding.value
+              // 为避免重复触发 stop() 停止监听元素的交叉情况
               stop()
             }
-          },
+          }
         )
       }
     })
