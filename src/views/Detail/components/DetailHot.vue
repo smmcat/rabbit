80973018212fb16a8b2d839f367efcdc07150d89
smmcat <1365116009@qq.com>
2024-05-24 11:24:43 +0800

    商品详情 放大镜等业务

---
+++ src/views/Detail/components/DetailHot.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { computed, ref } from 'vue'
+import { fetchHotGoodsAPI } from '@/apis/detail'
+import { useRoute } from 'vue-router'
+
+// type适配不同类型热榜数据
+const props = defineProps({
+  type: {
+    type: Number,
+    default: 1
+  }
+})
+
+// 标题类型字典
+const TITLEMAP = {
+  1: '24小时热榜',
+  2: '周热榜',
+  3: '总热销榜'
+}
+
+// 标题
+const title = computed(() => TITLEMAP[props.type])
+
+const hotList = ref([])
+const route = useRoute()
+const getHotList = async () => {
+  const res = await fetchHotGoodsAPI({
+    id: route.params.id,
+    type: props.type
+  })
+  hotList.value = res.result
+}
+getHotList()
+</script>
+
+
+<template>
+  <div class="goods-hot">
+    <h3>{{ title }}</h3>
+    <!-- 商品区块 -->
+    <RouterLink :to="`/detail/${item.id}`" class="goods-item" v-for="item in hotList" :key="item.id">
+      <img v-img-lazy="item.picture" alt="" />
+      <p class="name ellipsis">{{ item.name }}</p>
+      <p class="desc ellipsis">{{ item.desc }}</p>
+      <p class="price">&yen;{{ item.price }}</p>
+    </RouterLink>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+.goods-hot {
+  h3 {
+    height: 70px;
+    background: $helpColor;
+    color: #fff;
+    font-size: 18px;
+    line-height: 70px;
+    padding-left: 25px;
+    margin-bottom: 10px;
+    font-weight: normal;
+  }
+
+  .goods-item {
+    display: block;
+    padding: 20px 30px;
+    text-align: center;
+    background: #fff;
+
+    img {
+      width: 160px;
+      height: 160px;
+    }
+
+    p {
+      padding-top: 10px;
+    }
+
+    .name {
+      font-size: 16px;
+    }
+
+    .desc {
+      color: #999;
+      height: 29px;
+    }
+
+    .price {
+      color: $priceColor;
+      font-size: 20px;
+    }
+  }
+}
+</style>
